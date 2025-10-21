96a9afb6a82b1ff778f9185f34c54eb8b520a4b9
centenohugo <hcienteno@gmail.com>
2025-10-21 16:56:29 +0200

    Close to finish exercise

--- Lab3 Flask I/microblog/model.py
+++ Lab3 Flask I/microblog/model.py
@@ -1,13 +1,17 @@
 class Post:
-    def __init__(self, post_id, user, text, timestamp):
+    def __init__(self, post_id, user, text, img,  timestamp, comments = []):
         self.post_id = post_id
         self.user = user
         self.text = text
+        self.img = img
         self.timestamp = timestamp
+        self.comments = comments
 
 
 class User:
-    def __init__(self, user_id, email, name):
+    def __init__(self, user_id, email, name, profile_pic, posts = []):
         self.user_id = user_id
         self.email = email
         self.name = name
+        self.posts = posts
+        self.profile_pic = profile_pic
